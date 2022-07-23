fc6de10680695ec7c1f8074cb7cc0d2576ed28b2
sharifulR <shoriful15-11605@diu.edu.bd>
2022-07-23 22:05:53 +0600

    Create entity with dao

---
+++ app/src/main/java/com/example/todolist/NoteDao.java
@@ -0,0 +1,20 @@
+package com.example.todolist;
+
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+    
+    @Delete
+    void delete(Note note);
+}
